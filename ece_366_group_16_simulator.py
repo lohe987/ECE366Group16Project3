e69b3b4a680a92c69f51c1f05c31f5c3c5c06ad2
alfredomrz <aalmara2@uic.edu>
2018-11-01 00:16:06 -0500

    Still need to fix CNTR0
    Changed the file for simulartor
    New Simulator found at sim_v2.
    Changed the test files for testing. old d_mem and i_mem are found in copies.

--- ece_366_group_16_simulator.py
+++ ece_366_group_16_simulator.py
@@ -16,85 +16,63 @@ def simulate(Instruction,Memory):
             print("*HLT*")
         elif (fetch[1:4] == "101"):  #INIT
             print("*INIT*")
-            fetch = fetch.replace("101 ","")
-            fetch = fetch.split("101")
-            R = int(fetch[0])
+            R = format(int(fetch[4:6], 2))
             print("R = ", R)
-            imm = int(fetch[])
+            imm = format(int(fetch[6:8], 2))
             Reg[R] = imm
             PC += 1
         elif (fetch[1:4] == "111"):    #ADDI
             print("*ADDI*")
-            fetch = fetch.replace("111","")
-            fetch = fetch.split(",")
-            R = int(fetch[0])
-            imm = int(fetch[1])
+            R = format(int(fetch[4:6], 2))
+            imm = format(int(fetch[6:8], 2))
             Reg[R] = Reg[R] + imm
             PC += 1
         elif (fetch[1:8] == "1111100"):    #ADDN
             print("*ADDN*")
-            fetch = fetch.replace("1111100","")
-            R = 3
-            Reg[R] = Reg[R] - 1
+            Reg[3] = Reg[3] - 1
             PC += 1
         elif (fetch[1:4] == "100"):    #ADD
             print("*ADD*")
-            fetch = fetch.replace("100","")
-            fetch = fetch.split(",")
-            Rx = int(fetch[0])
-            Ry = int(fetch[1])
+            Rx = format(int(fetch[4:6]),2)
+            Ry = format(int(fetch[6:8), 2])
             Reg[Rx] = Reg[Rx] + Reg[Ry]
             PC += 1
         elif (fetch[1:6] == "00000"):   #SUBR0
             print("*SUBR0*")
-            fetch = fetch.replace("00000","")
-            Ry = int(fetch[0])
+            Ry = format(int(fetch[6:8]),2)
             Reg[0] = Reg[0] - Reg[Ry]
             PC += 1
         elif (fetch[1:5] == "0001"):    #XOR
             print("*XOR*")
-            fetch = fetch.replace("0001","")
-            fetch = fetch.split(",")      
-            Rx = int(fetch[0])
-            Ry = int(fetch[1])
+            Rx = format(int(fetch[5:6]), 2)
+            Ry = format(int(fetch[6:8), 2))
             Reg[Rx] = Reg[Rx] ^ Reg[Ry]
             PC += 1
         elif (fetch[1:4] == "001"):    #LWD
             print("*LWD*")
-            fetch = fetch.replace("001","")
-            fetch = fetch.replace("(","")
-            fetch = fetch.replace(")","")
-            fetch = fetch.split(",")
-            Rx = int(fetch[0])
-            Ry = int(fetch[1])
+            Rx = format(int(fetch[4:6]), 2)
+            Ry = format(int(fetch[6:8]), 2)
             Reg[Rx] = Memory[Ry]
             PC += 1
         elif (fetch[1:4] == "011"):    #SWD
             print("*SWD*")
-            fetch = fetch.replace("011","")
-            fetch = fetch.replace("(","")
-            fetch = fetch.replace(")","")
-            fetch = fetch.split(",")
-            Rx = int(fetch[0])
-            Ry = int(fetch[1])
+            Rx = format(int(fetch[4:6]), 2)
+            Ry = format(int(fetch[6:8]), 2)
             Memory[Ry] = Reg[Rx]
             PC += 1
         elif (fetch[1:4] == "110"):    #SLE
             print("*SLE*")
-            fetch = fetch.replace("110","")
-            fetch = fetch.split(",")
-            Rx = int(fetch[0])
-            Ry = int(fetch[1])
+            Rx = format(int(fetch[4:6]), 2)
+            Ry = format(int(fetch[6:8]), 2)
             if( Reg[Rx] < Reg[Ry] ):
                 Reg[3] = 1
             else:
                 Reg[3] = 0
             PC += 1
-        elif (fetch[1:6] == "00001"):    #SLER
+        elif (fetch[1:6] == "00001"):    #SLER Need to double check
             print("*SLER*")
-            fetch = fetch.replace("00001","")
             Rx = 0
-            Ry = int(fetch[0])
+            Ry = format(int(fetch[6:8]), 2)
             if( Reg[Ry] < Reg[0] ):
                 Reg[3] = 1
             else:
@@ -107,9 +85,11 @@ def simulate(Instruction,Memory):
             PC = PC + imm
         elif (fetch[1:4] == "010"):    #JIF
             print("*JIF*")
-            fetch = fetch.replace("010","")
-            imm = int(fetch[0])
-            PC = PC + imm
+            imm = format(int(4:8]), 2)
+            if(R[3] == 1):
+                PC = PC + imm
+            else:
+                PC = PC + 1
 
     print("******** Simulation finished *********")
     print("Dynamic Instr Count: ",DIC)
